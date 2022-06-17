ba3f79ddfe47d5e93ad0fa41ad10ab630b08e82b
MaleBurs <maleburs@gmail.com>
2022-06-17 10:23:51 -0300

    Update editProjectModal.tsx

--- front-end-proyectos/src/components/UI/Projects/editProjectModal.tsx
+++ front-end-proyectos/src/components/UI/Projects/editProjectModal.tsx
@@ -72,13 +72,6 @@ const EditProjectModal = (props: EditProjectModalProps) => {
         return response
     }
 
-    const handleSubmitProductModal = async () =>{
-        setShowProductModal(false);
-    }
-
-    const onCloseProductoModal =async () => {
-        setShowProductModal(false);
-    }
 
     const validateProjectName = () =>{
         if (newProject.name != "" && newProject.name.length<=20)
@@ -88,10 +81,10 @@ const EditProjectModal = (props: EditProjectModalProps) => {
     }
 
     const submit = () => {
-        onCloseProductoModal();
         updateProjectUsingAPI();
         props.onRefresh();
-        props.onClose();
+        //props.onSubmit();
+        onClose();
 
     }
 
@@ -113,19 +106,11 @@ const EditProjectModal = (props: EditProjectModalProps) => {
             setFormValidation(true);
             console.log("entro");
         }
-            /*if (response.status === 200) {
-                onSubmit();
-            }*/
-            //onSubmit();
-        /*if (isADevelopProjectAndHasNOTAProductAssign)
-            setFormValidation(false);*/
+ 
     }
 
     const handleSubmit = async () => {
         validateProjectValues();
-        //if(isADevelopProjectAndHasNOTAProductAssign){
-            //setProductModal(true);
-        //}else 
         if(isFormValid){
             console.log("valid");
             /*if (response.status === 200) {
@@ -151,27 +136,6 @@ const EditProjectModal = (props: EditProjectModalProps) => {
     return (
         <Modal onClose={onClose} open={show} >
             <div className='absolute bg-gray-200  text-slate-800 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[120vh] h-[85vh] rounded-xl shadow-lg'>
-                
-                <Modal onClose={onCloseProductoModal} open={showProductModal}>
-                    <div className='absolute bg-gray-200  text-slate-800 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[70vh] h-[55vh] rounded-xl shadow-lg'>
-                        <Typography variant='h5' className={'m-10'}>Producto correspondiente al proyecto de desarrollo</Typography>
-                        <div className='flex mb-6 flex-row ml-[6vh]'>  
-                            <TextField required id="outlined-basic" name="productId" className='mr-8 w-80' label="Seleccione el producto asociado al proyecto" InputLabelProps={{ shrink: true}} variant="outlined" onChange={handleChangeText}/> 
-                        </div>
-                        <div className='flex mb-6 flex-row ml-[6vh]'>  </div>
-                        <div className="flex flex-row ml-[6vh]" >
-                            <div className="text-center mr-8 mb-6 w-52 border-2 border-slate-400  rounded-xl shadow-lg font-bold text-slate-800 hover:border-teal-600 hover:border-1 hover:bg-gray-200 hover:text-teal-600 transition-all duration-300 cursor-pointer" onClick={onCloseProductoModal} >
-                                <div className="m-4" > Cancelar</div>
-                            </div>
-                            <div className="w-10" ></div>
-                            <div className="text-center mr-8 mb-6 w-52  border-2 border-slate-400  rounded-xl shadow-lg font-bold text-slate-800 hover:border-teal-600 hover:border-1 hover:bg-gray-200 hover:text-teal-600 transition-all duration-300 cursor-pointer" onClick={handleSubmitProductModal}>
-                                <div className="m-4" > Siguiente</div>
-                        </div>
-                    </div>
-
-                    </div>
-                </Modal>
-
                 <Typography variant='h5' className={'m-10'}>Modifique el proyecto</Typography>
                 <div className='ml-10 flex flex-col items-center'>
                     <div className='flex mb-6 flex-row'>
@@ -203,7 +167,7 @@ const EditProjectModal = (props: EditProjectModalProps) => {
                         </div>
                         <div className="w-56" ></div>
                         <div className="text-center mr-8 mb-6 w-52  border-2 border-slate-400  rounded-xl shadow-lg font-bold text-slate-800 hover:border-teal-600 hover:border-1 hover:bg-gray-200 hover:text-teal-600 transition-all duration-300 cursor-pointer" onClick={handleSubmit}>
-                            <div className="m-4" > Editar Proyecto </div>
+                            <div className="m-4" > Editar </div>
                         </div>
                     </div>
 
